cc5a729385249358af40698d8e7134b2da05961b
lukitasnashe <luka.avila@duocuc.cl>
2026-05-15 03:34:26 -0400

    inicio de ms_embalaje

---
+++ src/main/java/com/sivebo/ms_embalaje/MsEmbalajeApplication.java
@@ -0,0 +1,12 @@
+package com.sivebo.ms_embalaje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEmbalajeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEmbalajeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sivebo/ms_embalaje/MsEmbalajeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sivebo.ms_embalaje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEmbalajeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

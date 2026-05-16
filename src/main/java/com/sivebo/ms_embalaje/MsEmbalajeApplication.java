617e47ce91d0395a93c15068668f75b2023a88f7
lukitasnashe <luka.avila@duocuc.cl>
2026-05-15 22:49:04 -0400

    msembajaleApplication modificado

--- src/main/java/com/sivebo/ms_embalaje/MsEmbalajeApplication.java
+++ src/main/java/com/sivebo/ms_embalaje/MsEmbalajeApplication.java
@@ -2,11 +2,18 @@ package com.sivebo.ms_embalaje;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class MsEmbalajeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsEmbalajeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MsEmbalajeApplication.class, args);
+    }
+
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
 }
